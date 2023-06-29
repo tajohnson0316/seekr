f4bec742f506e3c09b15edf42c0f2771299519d0
tajohnson0316 <armanijohnson1331@gmail.com>
2023-06-29 13:15:40 -0500

    Initial commit

---
+++ src/main/java/com/tajohnson/seekr/SeekrApplication.java
@@ -0,0 +1,13 @@
+package com.tajohnson.seekr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeekrApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SeekrApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/tajohnson/seekr/SeekrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tajohnson.seekr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeekrApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
